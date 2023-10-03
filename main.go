b197dbdc05d8566b2e44b57f72ab13a0bea14c30
Mehak <mehakzehra5@gmail.com>
2023-10-03 06:29:44 -0700

    up;o

--- main.go
+++ main.go
@@ -29,6 +29,10 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"received_data": requestData.Data})
 	})
 
-	// Start the web server on port 8080.
 	r.Run(":8080")
 }
+
+//inatallation of Gin "go get -u github.com/gin-gonic/gin"
+
+//I have error handling for the POST request. If there's an issue with the JSON data or binding, it will return a 400 Bad Request response with an error message. Make sure your JSON data is correctly formatted.
+//
