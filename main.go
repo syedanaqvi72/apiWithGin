f4e88936c6af89341ab601469f0aadd1bae12731
Mehak <mehakzehra5@gmail.com>
2023-10-03 06:09:18 -0700

    add

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello")
+	// Initialize a Gin router.
+	r := gin.Default()
+
+	// Define a route that responds to GET requests.
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+	})
+
+	// Define a route that responds to POST requests.
+	r.POST("/postdata", func(c *gin.Context) {
+		var requestData struct {
+			Data string `json:"data"`
+		}
+
+		if err := c.BindJSON(&requestData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"received_data": requestData.Data})
+	})
+
+	// Start the web server on port 8080.
+	r.Run(":8080")
 }
